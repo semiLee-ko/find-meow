5f340caf6e512cbbc2623f007106e8d114ad1917
semiLee-ko <yanjisuca@gmail.com>
2025-12-12 16:20:40 +0900

    GitHub Actions 설정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
     },
+    base: './', // GitHub Pages 배포를 위한 상대 경로 설정
     build: {
         outDir: 'dist',
         emptyOutDir: true,
